8fc791c9f3df8e0d506f4deda8fe987af31ca0d9
tdmonster99 <jzegar2@gmail.com>
2026-04-25 17:04:12 -0500

    fix: throw clear error when FIREBASE_SERVICE_ACCOUNT_JSON is missing

--- api/_lib/firebase-admin.js
+++ api/_lib/firebase-admin.js
@@ -5,6 +5,8 @@ function getAdmin() {
   if (!_admin) {
     _admin = require('firebase-admin');
     if (!_admin.apps.length) {
+      if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON)
+        throw new Error('FIREBASE_SERVICE_ACCOUNT_JSON is not set');
       const sa = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
       _admin.initializeApp({ credential: _admin.credential.cert(sa) });
     }
